9c11144928f7159d394425cf6c8775d643769ef1
juniorpinhodev <juniorpinhodev@gmail.com>
2024-03-28 18:21:05 -0300

    update button play/pause and button exercise completed

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ let timer;
 let minutos = 0;
 let segundos = 3;
 let exercicioConcluido = 0;
+let isPaused = true; // Adicionado para controlar o estado de pausa
 
 const botaoExercicio = document.getElementById('exercise_completed');
-const startButton = document.getElementById('start');
-const pauseButton = document.getElementById('pause');
+const playPauseButton = document.getElementById('playPauseButton');
 const resetButton = document.getElementById('reset');
 const contador = document.getElementById('contador');
 const nameExercicio = document.getElementById('name_exercicio');
@@ -17,63 +17,60 @@ const descricaoExercicio = document.getElementById('descricao_exercicio');
 const exercicioConcluidoButton = document.getElementById('exercise_completed');
 const exercicioConcluidoText = document.getElementById('exercicio_concluido');
 
-
-botaoExercicio.addEventListener('click', () => {
-  exibirExercicio();
-  if(exercicioAtual === 9){
-    offset = offset + 10;
-    exercicioAtual = 0;
-    getExercises();
-    return;
-  }
-  exercicioAtual++;
-});
+botaoExercicio.addEventListener('click', togglePlayPause);
 
 function startPomodoro() {
-  startButton.disabled = true;
-  pauseButton.disabled = false;
+  playPauseButton.disabled = false; // Habilitar o botão de pause
   resetButton.disabled = false;
 
-  // Limpar as informações exibidas
-  contador.innerText = "";
-  nameExercicio.innerText = "";
-  dificuldadeExercicio.innerText = "";
-  descricaoExercicio.innerText = "";
+  // Verificar se o timer já está em execução antes de iniciar um novo
+  if (!timer) {
+    timer = setInterval(() => {
+      if (!isPaused) { // Verificar se não está pausado
+        if (segundos === 0) {
+          if (minutos === 0) {
+            clearInterval(timer);
+            playFinalSound(); // Reproduzir som ao final do ciclo
+            // Mostrar exercício
+            exibirExercicio();
+            minutos = 0;
+            segundos = 3;
+            playPauseButton.disabled = true; // Desabilitar o botão de pause
+            resetButton.disabled = true;
+            getExercises(); // Fazer nova requisição à API
+          } else {
+            minutos--;
+            segundos = 59;
+          }
+        } else {
+          segundos--;
+        }
 
-  timer = setInterval(() => {
-    if (segundos === 0) {
-      if (minutos === 0) {
-        clearInterval(timer);
-        playFinalSound(); // Reproduzir som ao final do ciclo
-        // Mostrar exercício
-        exibirExercicio();
-        minutos = 0;
-        segundos = 3;
-        startButton.disabled = false;
-        pauseButton.disabled = true;
-        resetButton.disabled = true;
-      } else {
-        minutos--;
-        segundos = 59;
+        document.getElementById('minutos').innerText = minutos.toString().padStart(2, '0');
+        document.getElementById('segundos').innerText = segundos.toString().padStart(2, '0');
       }
-    } else {
-      segundos--;
-    }
+    }, 1000);
+  }
+}
 
-    document.getElementById('minutos').innerText = minutos.toString().padStart(2, '0');
-    document.getElementById('segundos').innerText = segundos.toString().padStart(2, '0');
-  }, 1000);
+
+function togglePlayPause() {
+  isPaused = !isPaused; // Inverter o estado de pausa
+  if (isPaused) {
+    playPauseButton.classList.remove('ph-pause-circle');
+    playPauseButton.classList.add('ph-play-circle');
+  } else {
+    playPauseButton.classList.remove('ph-play-circle');
+    playPauseButton.classList.add('ph-pause-circle');
+    clearInterval(timer); // Limpar o timer atual
+    timer = null; // Reiniciar o timer
+    startPomodoro(); // Iniciar o novo timer
+  }
 }
 
 function playFinalSound() {
   const finalSound = new Audio('assets/Bubble Bell Sound effect.mp3');
   finalSound.play();
-}
-
-function pausePomodoro() {
-  clearInterval(timer);
-  startButton.disabled = false;
-  pauseButton.disabled = true;
 }
 
 function resetPomodoro() {
@@ -82,12 +79,11 @@ function resetPomodoro() {
   segundos = 3;
   document.getElementById('minutos').innerText = minutos.toString().padStart(2, '0');
   document.getElementById('segundos').innerText = segundos.toString().padStart(2, '0');
-  startButton.disabled = false;
-  pauseButton.disabled = true;
+  playPauseButton.classList.remove('ph-pause-circle'); // Resetar o ícone do botão
+  playPauseButton.classList.add('ph-play-circle');
+  playPauseButton.disabled = false;
   resetButton.disabled = true;
 }
-
-
 
 function exibirExercicio() {
   contador.innerText = exercicioAtual;
@@ -102,12 +98,10 @@ function exibirExercicio() {
   }
 }
 
-
-
 function getExercises(){
   fetch("https://api.api-ninjas.com/v1/exercises?type=stretching&offset=" + offset,{
     method: 'GET',
-    headers: { 'X-Api-Key': 'COLAR-API_AQUI'},
+    headers: { 'X-Api-Key': 'r5MkIilLYj0N864W9tgEcF3AYgmhiNUSeQ8lR3xF'},
     contentType: 'application/json',
   })
   .then(response => response.json())
@@ -116,8 +110,23 @@ function getExercises(){
   })
   .catch(error => console.log(error));
 }
+getExercises();
 
 exercicioConcluidoButton.addEventListener('click', () => {
+  togglePlayPause(); // Iniciar o timer
+
+  // Incrementar o exercício atual e verificar se precisa obter uma nova lista de exercícios
+  if (exercicioAtual === listaExercicios.length - 1) {
+    offset += 10; // Incrementar o offset para obter novos exercícios da API
+    exercicioAtual = 0; // Resetar o índice do exercício atual
+    getExercises(); // Obter uma nova lista de exercícios da API
+  } else {
+    exercicioAtual++; // Incrementar o exercício atual
+  }
+
+  // Exibir o próximo exercício na tela
+  exibirExercicio();
+
   exercicioConcluido++;
   exercicioConcluidoText.innerText = ` You completed ${exercicioConcluido} exercise`;
   // Limpar as informações exibidas
@@ -125,7 +134,8 @@ exercicioConcluidoButton.addEventListener('click', () => {
   nameExercicio.innerText = "";
   dificuldadeExercicio.innerText = "";
   descricaoExercicio.innerText = "";
-  startPomodoro(); // Reiniciar o contador
 });
 
-getExercises();
+
+
+
